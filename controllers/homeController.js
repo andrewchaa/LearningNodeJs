90fdbd9eb9fca1dd4d3149dea42e3d7843c90b2f
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2014-06-11 19:52:27 +0100

    use controllers

---
+++ controllers/homeController.js
@@ -0,0 +1,9 @@
+(function(homeController) {
+
+	homeController.init = function(app) {
+		app.get("/", function(req, res) {
+			res.render("index", { title: "express " + "engine" });
+		});
+	};
+
+})(module.exports);
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var http = require("http");
 var express = require("express");
 var app = express();
+var controllers = require("./controllers");
 
 app.set("view engine", "vash");
+controllers.init(app);
+// app.get("/", function(req, res) {
+// 	res.render("index", { title: "express" + "vash" });
+// });
 
-app.get("/", function(req, res) {
-	res.render("index", { title: "express" + "vash" });
-});
+
 
 var server = app.listen("3000", function() {
 	console.log("listening on port %d", server.address().port);
