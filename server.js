38fcddf8d6f9b8c0eadc209ebda88f43be3015de
andrew.chaa <andrew.chaa@huddle.com>
2014-06-13 11:20:38 +0100

    use grunt and grunt-nodemon

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var controllers = require("./controllers");
 app.set("view engine", "vash");
 app.use(express.static(__dirname + "/public"));
 
-controllers.init(app);
+controllers.init(app); 
 
 var server = app.listen("3000", function() {
 	console.log("listening on port %d", server.address().port);
