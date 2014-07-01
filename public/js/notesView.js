6c53132f4407968b5e2c192e1335ef9790d81864
andrew.chaa <andrew.chaa@huddle.com>
2014-07-01 11:59:39 +0100

    use angular window and http objects

--- public/js/notesView.js
+++ public/js/notesView.js
@@ -4,26 +4,21 @@
     
     theModule.controller("notesViewController",
         [
-            "$scope",
-            function ($scope) {
-                $scope.notes = [
-                    {
-                        note : "Hello world 1",
-                        author : "Andy",
-                        colour : "blue"
-                    }
-                    ,
-                    {
-                        note : "Hello world 2",
-                        author : "Sue",
-                        colour : "red"
-                    },
-                    {
-                        note : "Hello world 3",
-                        author : "Joel",
-                        colour : "purple"
-                    }
-                ];
+            "$scope", "$window", "$http",
+            function ($scope, $window, $http) {
+                var urlParts = $window.location.pathname.split("/");
+                var categoryName = urlParts[urlParts.length - 1];
+                
+                $http.get("/api/notes/" + categoryName)
+                    .then(
+                        function (result) {
+                            console.log(result);
+                            $scope.notes = result.data.notes;
+                        },
+                        function (error) {
+                            console.log(error);
+                        }
+                    );
             }
         ]);
         
